12258d9430e4978d836de540acd358a5f53b3146
zeloras <zeloras@gmail.com>
2019-07-08 09:16:53 +0300

    permission to static

--- Http/routes.php
+++ Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => ['web', 'permission:admin_access'], 'prefix' => getAdminPrefix('translates')], function () {
+Route::group(['middleware' => ['web', 'permission:' . \Gcms::MAIN_ADMIN_PERMISSION], 'prefix' => getAdminPrefix('translates')], function () {
     Route::group(['middleware' => ['permission:modules_translates_admin_list']], function () {
         Route::get('/', 'GeekCms\Translates\Http\Controllers\AdminController@index')
             ->name('admin.translates')
